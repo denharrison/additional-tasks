243008d368fdf4d8a0190e3148b26526fa000880
Denis Harrison <denharrison797@gmail.com>
2025-11-07 17:43:56 +0300

    added task 7

--- task6.js
+++ task6.js
@@ -5,10 +5,10 @@ const manualReverse = (array) => {
     const n = array.length;
     for (let i = 0; i < n / 2; i++) {
 
-      const temp = array[i];
-      array[i] = array[n - 1 - i];
-      array[n - 1 - i] = temp;
-      
+      const temp = array[i]; // temp = 1 
+      array[i] = array[n - 1 - i]; // array[0] = array[4 - 1 - 0] (3) array[0] = 3 следовательно 4 (по индексам считаем)
+      array[n - 1 - i] = temp; // array[3] = 1 - тут мы меняем 3 индекс на единичку
+
     }
 
     return array
